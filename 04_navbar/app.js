ab59626eccb35e7ae32f8e2e94fcdf8280a18f69
sebbyapudo <sebbyapudo@gmail.com>
2023-04-21 01:09:37 +0300

    Refactored the code to write shorter and concise code

--- 04_navbar/app.js
+++ 04_navbar/app.js
@@ -10,10 +10,11 @@ const links = document.querySelector('.links');
 
 navToggle.addEventListener('click', ()=>{
   // console.log(links.classList);
-  if(links.classList.contains('show-links')) {
-    links.classList.remove('show-links');
-  }
-  else {
-    links.classList.add('show-links');
-  }
+  // if(links.classList.contains('show-links')) {
+  //   links.classList.remove('show-links');
+  // }
+  // else {
+  //   links.classList.add('show-links');
+  // }
+  links.classList.toggle('show-links');
 })
