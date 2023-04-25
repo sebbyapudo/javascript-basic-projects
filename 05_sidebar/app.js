95567178a258cde18b723dc6ece918a0e60a3b06
sebbyapudo <sebbyapudo@gmail.com>
2023-04-25 22:26:02 +0300

    Set up the functionality that toggles the side bar

---
+++ 05_sidebar/app.js
@@ -0,0 +1,19 @@
+// Declaring the essential variables
+
+const toggleBtn = document.querySelector('.sidebar-toggle');
+const closeBtn = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar')
+
+toggleBtn.addEventListener('click', ()=>{
+  // console.log(sidebar.classList);
+  if(sidebar.classList.contains('show-sidebar')) {
+    sidebar.classList.remove('show-sidebar')
+  } 
+  else {
+    sidebar.classList.add('show-sidebar')
+  }
+})
+
+closeBtn.addEventListener('click', ()=>{
+  sidebar.classList.toggle('show-sidebar')
+})
