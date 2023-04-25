acc3e8e327da7a017345743bc01e4a14674ee2d3
sebbyapudo <sebbyapudo@gmail.com>
2023-04-25 23:41:14 +0300

    Refactored the code to be more short and concise

--- 05_sidebar/app.js
+++ 05_sidebar/app.js
@@ -6,14 +6,15 @@ const sidebar = document.querySelector('.sidebar')
 
 toggleBtn.addEventListener('click', ()=>{
   // console.log(sidebar.classList);
-  if(sidebar.classList.contains('show-sidebar')) {
-    sidebar.classList.remove('show-sidebar')
-  } 
-  else {
-    sidebar.classList.add('show-sidebar')
-  }
+  // if(sidebar.classList.contains('show-sidebar')) {
+  //   sidebar.classList.remove('show-sidebar')
+  // } 
+  // else {
+  //   sidebar.classList.add('show-sidebar')
+  // }
+  sidebar.classList.toggle('show-sidebar');
 })
 
 closeBtn.addEventListener('click', ()=>{
-  sidebar.classList.toggle('show-sidebar')
+  sidebar.classList.remove('show-sidebar')
 })
