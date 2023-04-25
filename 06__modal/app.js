2e6e8e8e4148a793c57380b29ae74f973642594f
sebbyapudo <sebbyapudo@gmail.com>
2023-04-26 00:40:56 +0300

    Refactor code to make it dry

--- 06__modal/app.js
+++ 06__modal/app.js
@@ -8,12 +8,7 @@ const modalOverlay = document.querySelector('.modal-overlay');
 const closeBtn = document.querySelector('.close-btn');
 
 modalBtn.addEventListener('click', ()=>{
-  if(modalOverlay.classList.contains('open-modal')) {
-    modalOverlay.classList.remove('open-modal')
-  }
-  else {
-    modalOverlay.classList.add('open-modal')
-  }
+  modalOverlay.classList.toggle('open-modal')
 })
 
 closeBtn.addEventListener('click', ()=>{
