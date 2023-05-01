d7af89bee02e0ebfca2c5e515f4c64232a032479
sebbyapudo <sebbyapudo@gmail.com>
2023-05-01 19:33:20 +0300

    Added the accordion functionality that closes a separate question when another is opened

--- 07_questions/app.js
+++ 07_questions/app.js
@@ -7,6 +7,14 @@ questions.forEach((question)=>{
   const btn = question.querySelector('.question-btn')
   // console.log(btn);
   btn.addEventListener('click', ()=>{
+    questions.forEach((item)=>{
+      // console.log(item);
+      if(item !== question) {
+        item.classList.remove('show-text')
+      }
+    })
+
+
     question.classList.toggle('show-text');
   })
 })
