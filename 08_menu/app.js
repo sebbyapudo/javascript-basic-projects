3caea12f404591f75dccd27a77de7f8dfaa9fe85
sebbyapudo <sebbyapudo@gmail.com>
2023-05-03 01:00:12 +0300

    Set up the functionality that populates the menu with the menu-items

--- 08_menu/app.js
+++ 08_menu/app.js
@@ -72,3 +72,26 @@ const menu = [
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
+
+const sectionCenter = document.querySelector('.section-center');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+  let displayMenu = menu.map((item)=>{
+    // console.log(item);
+    return `<article class="menu-item">
+      <img src=${item.img} class="photo" alt=${item.title}>
+      <div class="item-info">
+        <header>
+          <h4>${item.title}</h4>
+          <h4 class="price">Ksh ${item.price}</h4>
+        </header>
+        <p class="item-text">
+          ${item.desc}
+        </p>
+      </div>
+    </article>`;
+  })
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu
+  
+});
