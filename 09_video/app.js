f1f5ada06f69a500e6920aed4b43f05f2d4b218a
sebbyapudo <sebbyapudo@gmail.com>
2023-05-15 01:49:47 +0300

    Implemented the preloader functionality

--- 09_video/app.js
+++ 09_video/app.js
@@ -8,8 +8,17 @@ const video = document.querySelector('.video-container');
 btn.addEventListener('click', ()=>{
   if(!btn.classList.contains('slide')) {
     btn.classList.add('slide');
+    video.pause()
   }
   else {
     btn.classList.remove('slide');
+    video.play()
   }
+});
+
+// preloader
+const preloader = document.querySelector('.preloader');
+
+window.addEventListener('load', ()=>{
+  preloader.classList.add('hide-preloader')
 });
