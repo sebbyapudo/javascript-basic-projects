5162186f8a304108d656c5bfdeb83dcfb9c738d3
sebbyapudo <sebbyapudo@gmail.com>
2023-05-21 10:35:22 +0300

    Implemented the filter functionality on the article btns

---
+++ 11_tabs/app.js
@@ -0,0 +1,22 @@
+// Declaring global variables
+const btns = document.querySelectorAll('.tab-btn');
+const about = document.querySelector('.about');
+const articles = document.querySelectorAll('.content');
+
+about.addEventListener('click', (e)=>{
+  const id = e.target.dataset.id;
+
+  if(id) {
+    // remove the active class from other btns
+    btns.forEach((btn)=>{
+      btn.classList.remove('active')
+      e.target.classList.add('active')
+    });
+    // hide other articles
+    articles.forEach((article)=>{
+      article.classList.remove('active')
+    });
+    const element = document.getElementById(id);
+    element.classList.add('active')
+  }
+})
