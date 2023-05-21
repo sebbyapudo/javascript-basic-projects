12b1c9e2a3a44dd5959d3ee99275150742cd5d66
sebbyapudo <sebbyapudo@gmail.com>
2023-05-21 08:32:24 +0300

    Implemented the scroll functionality that scrolls the page to the top of a section

--- 10_scroll/app.js
+++ 10_scroll/app.js
@@ -52,4 +52,27 @@ window.addEventListener('scroll', ()=>{
 
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
 
+scrollLinks.forEach(function(link){
+  link.addEventListener('click', (e)=>{
+    // prevent default
+    e.preventDefault();
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+
+    // calculate the heights
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixNav = navbar.classList.contains("fixed-nav")
+    let position = element.offsetTop - navHeight;
+    console.log(position);
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    })
+    linksContainer.style.height = 0;
+  })
+})
