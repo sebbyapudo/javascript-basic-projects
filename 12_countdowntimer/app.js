52113f143e0e7b367fad8e38ab20be66a67eb671
sebbyapudo <sebbyapudo@gmail.com>
2023-06-01 22:42:29 +0300

    Extracted the individual days, hours, mins and seconds values

--- 12_countdowntimer/app.js
+++ 12_countdowntimer/app.js
@@ -29,7 +29,6 @@ const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
 let futureDate = new Date(2023, 6, 12, 11, 23, 0);
-console.log(futureDate);
 
 const year = futureDate.getFullYear(); 
 const hours = futureDate.getHours();
@@ -41,4 +40,31 @@ const date = futureDate.getDate();
 let weekday = weekdays[futureDate.getDay()];
 
 
-giveaway.textContent = `giveaway ends on ${weekday} ${date} ${month} ${year} ${hours}:${minutes}am`
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
+
+// future time in ms
+
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  console.log(t);
+  // 1s --> 1000ms
+  // 1m --> 60s
+  // 1hr --> 60mins
+  // 1day --> 24hr
+
+  // Values in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  // calculate all values
+  let days = Math.floor(t / oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let mins = Math.floor((t % oneHour)  / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+  console.log(mins);
+}
+
+getRemainingTime();
