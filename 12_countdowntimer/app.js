940d7a45ba84e5357feab8ed801078c9cab63d15
sebbyapudo <sebbyapudo@gmail.com>
2023-06-01 21:58:11 +0300

    Finished setting up the future date in the giveaway section

--- 12_countdowntimer/app.js
+++ 12_countdowntimer/app.js
@@ -21,3 +21,26 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+
+// Declaring global variables
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2023, 6, 12, 11, 23, 0);
+console.log(futureDate);
+
+const year = futureDate.getFullYear(); 
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+// setting up the month and weekdays
+let month = futureDate.getMonth();
+month = months[month]
+const date = futureDate.getDate();
+
+let weekday = futureDate.getDay();
+weekday = weekdays[weekday]
+
+giveaway.textContent = `giveaway ends on ${weekday} ${date} ${month} ${year} ${hours}:${minutes}am`
