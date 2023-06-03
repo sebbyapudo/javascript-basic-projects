10cf82228fad42703a460ea62ab3eb48328b8b44
sebbyapudo <sebbyapudo@gmail.com>
2023-06-03 19:49:07 +0300

    Set up the logic & alert for an empty submission || value is empty

--- 14_grocery-bud/app.js
+++ 14_grocery-bud/app.js
@@ -26,10 +26,20 @@ function addItem(e) {
   }else if (value && editFlag){
     console.log("editing");
   }else{
-    alert.textContent = 'empty value';
-    alert.classList.add('alert-danger')
+   displayAlert('Please enter value', 'danger')
   }
 }
+// display alerts
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`)
+  // remove alert
+  setTimeout(function() {
+    alert.textContent = '';
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
+}
+
 
 // ****** LOCAL STORAGE **********
 
